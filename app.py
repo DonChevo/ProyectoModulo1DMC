95920b4597c2d5123f11cef73dc79f183b6c9f93
DonChevo <47583871+DonChevo@users.noreply.github.com>
2026-07-09 22:23:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ st.title("Proyecto Módulo I Fundamentals")
 st.sidebar.title("Parámetros")
 st.image("Image20260709220523.png")
 st.sidebar.image("Image20260709220550.png")
-valor_inicial = st.number_input("Ingrese el valor inicial", value = 0)
-valor_final = st.number_input("Ingrese el valor final", value = 1)
 
-lista_numerica = list(range(valor_inicial, valor_final))
-
-st.write(lista_numerica)
+modulo = st.sidebar.selectbox("Elija modulo",[Modulo Listas],[Modulo Array],[Modulo Funciones])
+if modulo == "Modulo Listas":
+  valor_inicial = st.number_input("Ingrese el valor inicial", value = 0)
+  valor_final = st.number_input("Ingrese el valor final", value = 1)
+  lista_numerica = list(range(valor_inicial, valor_final))
+  st.write(lista_numerica)
+elif modulo == "Modulo Array"
+  st.write("Hola")
+else
+  st.write("Hola2")
+  
