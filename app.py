4283d5cd9f86cee713cf29a6d0dfdda4bc7af0f9
DonChevo <47583871+DonChevo@users.noreply.github.com>
2026-07-09 22:25:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.sidebar.title("Parámetros")
 st.image("Image20260709220523.png")
 st.sidebar.image("Image20260709220550.png")
 
-modulo = st.sidebar.selectbox("Elija modulo",[Modulo Listas],[Modulo Array],[Modulo Funciones])
+modulo = st.sidebar.selectbox("Elija modulo",[Modulo Listas,Modulo Array,Modulo Funciones])
 if modulo == "Modulo Listas":
   valor_inicial = st.number_input("Ingrese el valor inicial", value = 0)
   valor_final = st.number_input("Ingrese el valor final", value = 1)
