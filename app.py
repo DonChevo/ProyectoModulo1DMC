a2e85fa43c3906934a9156a4faf447dab079423a
DonChevo <47583871+DonChevo@users.noreply.github.com>
2026-07-09 22:16:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title("Proyecto Módulo I Fundamentals")
 st.sidebar.title("Parámetros")
 st.image("Image20260709220523.png")
-
+st.sidebar.image("Image20260709220550.png")
 valor_inicial = st.number_input("Ingrese el valor inicial", value = 0)
 valor_final = st.number_input("Ingrese el valor final", value = 1)
 
